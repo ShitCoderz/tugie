fb4accc7d5ca60bfb2e744841b64549caf2a5790
Yota <luzan.k.work@gmail.com>
2025-02-13 16:28:41 +0500

    Added content

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import "./App.css";
+import s from "./App.module.css";
+import TugieAudio from "./assets/audio/tugie.mp3";
+import Asterisk from "./assets/images/asterisk.png";
 
 function App() {
+  const audioPlayer = new Audio(TugieAudio);
+  audioPlayer.loop = true;
+  audioPlayer.autoplay = true;
+  const playAudio = () => {
+    audioPlayer.play();
+  };
+
   return (
     <>
-      <h1>Тугие Ануса</h1>
+      <main className={s["tugie"]}>
+        <h1 className={s["tugie__title"]}>Тугие Ануса</h1>
+        <div className={s["tugie__circle"]} onClick={playAudio}>
+          <img
+            className={s["tugie__asterisk"]}
+            width={48}
+            height={48}
+            src={Asterisk}
+          />
+        </div>
+      </main>
     </>
   );
 }
