604e8714dfee046cb07db6c766075dd963773501
Yota <luzan.k.work@gmail.com>
2025-02-13 18:41:56 +0500

    Pulse

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,67 @@
+import { useRef } from "react";
 import s from "./App.module.css";
 import TugieAudio from "./assets/audio/tugie.mp3";
 import Asterisk from "./assets/images/asterisk.png";
 
 function App() {
-  const audioPlayer = new Audio(TugieAudio);
-  audioPlayer.loop = true;
-  audioPlayer.autoplay = true;
+  const circleElement = useRef<HTMLDivElement>(null);
+  let isPlaying = false;
+
+  const audioElement = new Audio(TugieAudio);
+  audioElement.loop = true;
+
   const playAudio = () => {
-    audioPlayer.play();
+    if (isPlaying) return;
+    isPlaying = true;
+
+    const audioContext = new AudioContext();
+    const analyser = audioContext.createAnalyser();
+    const source = audioContext.createMediaElementSource(audioElement);
+    source.connect(analyser);
+    analyser.connect(audioContext.destination);
+
+    analyser.fftSize = 256;
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+
+    audioElement.play();
+    animate();
+
+    function animate() {
+      requestAnimationFrame(animate);
+      analyser.getByteTimeDomainData(dataArray);
+
+      let sum = 0;
+      for (let i = 0; i < bufferLength; i++) {
+        sum += Math.abs(dataArray[i] - 128);
+      }
+      const averageAmplitude = sum / bufferLength;
+
+      if (averageAmplitude > 25) {
+        pulseVisualizer();
+      }
+    }
+
+    function pulseVisualizer() {
+      if (circleElement.current) {
+        circleElement.current.style.transform = "scale(1.25)";
+        setTimeout(
+          () => (circleElement.current!.style.transform = "scale(1)"),
+          200
+        );
+      }
+    }
   };
 
   return (
     <>
       <main className={s["tugie"]}>
         <h1 className={s["tugie__title"]}>Тугие Ануса</h1>
-        <div className={s["tugie__circle"]} onClick={playAudio}>
+        <div
+          className={s["tugie__circle"]}
+          onClick={playAudio}
+          ref={circleElement}
+        >
           <img
             className={s["tugie__asterisk"]}
             width={48}
